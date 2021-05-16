a5d05969fa14516f26c7af0c020263c4575abb3b
Joshua Rupp <j.rupp@onecue.de>
2021-05-16 15:05:41 +0200

    Projektdateien hinzufügen.

---
+++ CrashTest/CrashTest/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashTest
+{
+    class TestModel
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string E { get; set; }
+        public string F { get; set; }
+        public string G { get; set; }
+        public string H { get; set; }
+        public string I { get; set; }
+        public string J { get; set; }
+        public string K { get; set; }
+        public string L { get; set; }
+    }
+}
